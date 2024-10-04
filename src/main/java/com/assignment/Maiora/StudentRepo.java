0d56cecaa7f38b9c3932ce1d7503f4c76d6ba080
shobanasekar0423 <68852851+shobanasekar0423@users.noreply.github.com>
2024-10-04 17:20:00 +0530

    Update StudentRepo.java

--- src/main/java/com/assignment/Maiora/StudentRepo.java
+++ src/main/java/com/assignment/Maiora/StudentRepo.java
@@ -3,6 +3,7 @@ package com.assignment.Maiora;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,4 +12,6 @@ public interface StudentRepo extends JpaRepository<Student, Integer> {
 	
 	public Student save(Student student);
 	public List<Student> findAll();
+	@Query("Select a from Student a where a.age>=?1 AND a.age<=?2")
+	public List<Student> findStudentBetweenAges(int fromAge, int toAge);
 }
